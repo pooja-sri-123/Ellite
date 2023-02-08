d63077c516e3e15f0b5eb4c25629bcece47570a7
pooja-sri-123 <84763644+pooja-sri-123@users.noreply.github.com>
2023-02-08 15:57:56 +0530

    Create bipartite.java

---
+++ 07-02-23/bipartite.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n= graph.length;
+        int[] visited = new int[n];
+        for(int i=0;i<n;i++){
+            if(visited[i] != 0) continue;
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(i);
+            visited[i]=1;
+
+            while(!queue.isEmpty()){
+                int curr =queue.poll();
+                for(int neigh: graph[curr]){
+                    if(visited[neigh] == 0) {
+                        visited[neigh] = -visited[curr];
+                        queue.add(neigh);
+                    }
+                    else if(visited[neigh]  != -visited[curr]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
