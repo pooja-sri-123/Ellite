2f8c1051b9e0aac765e9ef04c99300d83c0f1540
pooja-sri-123 <84763644+pooja-sri-123@users.noreply.github.com>
2023-02-09 23:40:48 +0530

    Detect cycle in directed graph by dfs

---
+++ 08-02-23/DirectedGraph.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        int[] visited = new int[V];
+        int[] predecessor = new int[V];
+        for(int i=0;i<V;i++){
+            if(visited[i] ==0){
+                if(dfs(i,predecessor,visited,adj) == true) return true;
+            }
+            
+        }
+        return false;
+        
+    }
+    public  boolean dfs(int source,int[] predecessor,int[] visited,ArrayList<ArrayList<Integer>>adj){
+        visited[source] =1;
+        predecessor[source]=1;
+        for(int neigh:adj.get(source)){
+            if(visited[neigh] == 0){
+                if(dfs(neigh,predecessor,visited,adj) == true) return true;
+            }
+            else if(predecessor[neigh] == 1) return true;
+        }
+        predecessor[source]=0;
+        return false;
+    }
+}
