cab38321df0732ddb203d0e64996e3322528268a
pooja-sri-123 <84763644+pooja-sri-123@users.noreply.github.com>
2023-02-12 13:25:05 +0530

    keys and rooms in dfds

---
+++ 11-02-23/keys and rooms.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] vis = new boolean[rooms.size()];
+        dfs(0,vis,rooms);
+        for(boolean val:vis){
+            if(!val) return false;
+        }
+        return true;
+
+    }
+    public static void dfs(int node,boolean[] vis,List<List<Integer>>rooms){
+        vis[node]=true;
+        for(int neigh:rooms.get(node)){
+            if(!vis[neigh]){
+                dfs(neigh,vis,rooms);
+            }
+        }
+    }
+}
