5683ad93fdd0c261b32515468e0520d6790c68c7
Pooja sri <84763644+pooja-sri-123@users.noreply.github.com>
2023-03-04 19:31:15 +0530

    finding middle of linkedlist

---
+++ 04-03-23/middle of likedlist.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int count=0;
+        ListNode curr=head;
+        while(curr!=null){
+            count++;
+            curr=curr.next;
+        }
+        curr=head;
+        for(int i=0;i<count/2;i++){
+            curr=curr.next;
+        }
+        return curr;
+    }
+}
